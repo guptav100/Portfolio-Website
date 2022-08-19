7dbc780d5ae6226139cdccc8dfec5d7a5f3f6e20
Vishal Gupta <90566964+guptav100@users.noreply.github.com>
2022-08-19 12:11:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout'
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Layout />} />  
+    </Routes>
+      </>
+  );
+}
+
+export default App;
